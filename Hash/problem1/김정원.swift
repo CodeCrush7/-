e9a9ff8fd82b997f95bb5b16b60a18f6b848084e
bluewhale0528 <47441767+bluewhale0528@users.noreply.github.com>
2020-02-15 13:49:27 +0900

    Update 김정원.swift

--- Hash/problem1/김정원.swift
+++ Hash/problem1/김정원.swift
@@ -20,13 +20,14 @@ func solution(participant: [String], completion: [String]) -> String {
     return answer
 }
 
-solution(participant: ["marina", "josipa", "nikola", "vinko", "filipa"], completion: ["josipa", "filipa", "marina", "nikola"])
-solution(participant: ["mislav", "stanko", "mislav", "ana"], completion: ["stanko", "ana", "mislav"])
-solution(participant: ["kelly"], completion: [])
-solution(participant: [], completion: [])
+solution(participant: ["marina", "josipa", "nikola", "vinko", "filipa"], completion: ["josipa", "filipa", "marina", "nikola"]) // "vinko"
+solution(participant: ["mislav", "stanko", "mislav", "ana"], completion: ["stanko", "ana", "mislav"]) // "mislav"
+solution(participant: ["kelly"], completion: []) // "kelly"
+solution(participant: [], completion: []) // "잘못 입력하셨습니다"
 
 // 여러분 저는 파이썬 말고 스위프트를 이용했어요...ㅎ 그래도 생각하는 흐름은 비슷할거니까..?ㅎㅎ
 
 // 우선 받은 참여자와 완주자 배열을 순서대로 정렬한 다음, for문을 완주자 명단의 count갯수만큼 돌면서 참여자명단이랑 완주자명단을 차례차례 비교했고 일치하지 않는 값이 나오면 그 값을 리턴하도록 했다. 
 // 이때 만약에 참여자명단의 마지막 사람이 완주하지 못했을 경우에는 인덱스로 비교해서 찾을 수가 없다. 왜냐하면 인덱스비교는 완주자명단 count를 기준으로 이미 끝났기 때문이다. 따라서 이 경우에는 참여자 명단의 맨 마지막 값을 리턴하도록 했다.
 // 만약 참여자가 한 명이고 그 사람이 완주를 하지 못한 경우에는 케이스를 따로 분리해서 그냥 참여자명단의 값을 리턴하도록 했다.
+// 그리고 제한사항도 첫 줄에 구현을 했는데, 참여자와 완주자가 한 명도 없는 경우이거나 참여자 수가 100,000명 이상이 되면 그냥 이 함수를 바로 빠져나가도록 처리했다.
